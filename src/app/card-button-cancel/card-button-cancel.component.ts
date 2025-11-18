c5b111cfd41c9a413b90bcc7c61f3ad65b70bc9a
thiago_osim <thiago_osim@hotmail.com>
2025-11-18 20:23:37 -0300

    Removido o card-button-cancel-2.component.scss

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel-2.component.scss']
+  styleUrls: ['./card-button-cancel.component.scss']
 })
 export class CardButtonCancelComponent {
 
