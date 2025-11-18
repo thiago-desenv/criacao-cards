5a86970d60741b068374a99cde71af5bd300b343
thiago_osim <thiago_osim@hotmail.com>
2025-11-18 20:21:35 -0300

    Criado estilo card-button-cancel-2.component.scss para exemplificar o uso de dois estilos scss no mesmo componente

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,26 +3,7 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrl: './card-button-cancel.component.scss',
-  styles: [
-    `
-      .card-cancel-button {
-        font-size: 16px;
-        text-align: center;
-        margin-top: 10px;
-        border-radius: 25px;
-        background-color: red;
-        cursor: pointer;
-        font-weight: bold;
-        padding: 10px;
-      }
-    `,
-    `
-      .card-cancel-button {
-        background-color: gray;
-      }
-    `
-  ]
+  styleUrls: ['./card-button-cancel.component.scss', './card-button-cancel-2.component.scss']
 })
 export class CardButtonCancelComponent {
 
