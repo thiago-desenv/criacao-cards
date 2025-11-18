5ae397c2c0b31e4b544fd84b3576c6c7b2e9c889
thiago_osim <thiago_osim@hotmail.com>
2025-11-18 20:28:28 -0300

    Demonstrado como aplicar estilos inline

--- src/app/card-button-cancel/card-button-cancel.component.ts
+++ src/app/card-button-cancel/card-button-cancel.component.ts
@@ -3,7 +3,14 @@ import { Component } from '@angular/core';
 @Component({
   selector: 'app-card-button-cancel',
   templateUrl: './card-button-cancel.component.html',
-  styleUrls: ['./card-button-cancel.component.scss']
+  styleUrls: ['./card-button-cancel.component.scss'],
+  styles: [
+    `
+      .card-cancel-button {
+        background-color: gray;
+      }
+    `
+  ]
 })
 export class CardButtonCancelComponent {
 
