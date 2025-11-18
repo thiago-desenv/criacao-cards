edd224bed2208fad64f920b2c229683d1fcfcbee
thiago_osim <thiago_osim@hotmail.com>
2025-11-17 21:48:53 -0300

    utilizado prop template

--- src/app/card-button/card-button.component.ts
+++ src/app/card-button/card-button.component.ts
@@ -2,7 +2,10 @@ import { Component } from '@angular/core';
 
 @Component({
   selector: 'app-card-button',
-  templateUrl: './card-button.component.html',
+  //templateUrl: './card-button.component.html',
+  template: `
+    <div class="card-button">Adquirir</div>
+  `,
   styleUrl: './card-button.component.scss'
 })
 export class CardButtonComponent {
--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -6,10 +6,8 @@ import { Component } from '@angular/core';
   styleUrl: './card.component.scss'
 })
 export class CardComponent {
-  plano = {
-    infos: {
-      tipo: 'Simples',
-      preco: 100
-    }
-  }
+  // @ts-ignore
+  plano: any = {
+    infos: undefined
+  };
 }
