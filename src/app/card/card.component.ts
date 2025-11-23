0ecf2d9f75b007ea076bd2bc9259053e39b29b1e
thiago_osim <thiago_osim@hotmail.com>
2025-11-23 19:57:50 -0300

    Usado encapsulation: ViewEncapsulation.None que é o comportamento padrão de estilização

--- src/app/card/card.component.ts
+++ src/app/card/card.component.ts
@@ -4,7 +4,7 @@ import { Component, ViewEncapsulation } from '@angular/core';
   selector: 'app-card',
   templateUrl: './card.component.html',
   styleUrl: './card.component.scss',
-  encapsulation: ViewEncapsulation.None
+  encapsulation: ViewEncapsulation.Emulated
 })
 export class CardComponent {
   // @ts-ignore
