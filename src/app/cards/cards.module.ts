a67c531a28e9ba96a0da2e12ca460ec66c0d7b32
thiago_osim <thiago_osim@hotmail.com>
2025-11-23 19:06:39 -0300

    Adicionado slider do angular material nos cards

--- src/app/cards/cards.module.ts
+++ src/app/cards/cards.module.ts
@@ -5,6 +5,7 @@ import { CardRoxoComponent } from '../card-roxo/card-roxo.component';
 import { CardButtonComponent } from '../card-button/card-button.component';
 import { CardRoxoButtonComponent } from '../card-roxo-button/card-roxo-button.component';
 import { CardButtonCancelComponent } from '../card-button-cancel/card-button-cancel.component';
+import { MatSliderModule} from '@angular/material/slider';
 
 @NgModule({
   declarations: [
@@ -15,7 +16,8 @@ import { CardButtonCancelComponent } from '../card-button-cancel/card-button-can
     CardButtonCancelComponent
   ],
   imports: [
-    CommonModule
+    CommonModule,
+    MatSliderModule
   ],
   exports: [
     CardComponent,
